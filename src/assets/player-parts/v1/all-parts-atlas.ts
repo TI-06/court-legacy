c2ab7045d735e1e713eb2e1cb4e8991b1dc7b4e4
TI-06 <iretaku.shop@gmail.com>
2026-07-31 11:15:18 +0900

    feat: assemble generated player art WebP atlas data URL

---
+++ src/assets/player-parts/v1/all-parts-atlas.ts
@@ -0,0 +1,10 @@
+import chunk0 from "./all-parts-atlas/chunk-0";
+import chunk1 from "./all-parts-atlas/chunk-1";
+import chunk23 from "./all-parts-atlas/chunk-23";
+import chunk45 from "./all-parts-atlas/chunk-45";
+import chunk67 from "./all-parts-atlas/chunk-67";
+import chunk8 from "./all-parts-atlas/chunk-8";
+
+const allPartsAtlasUrl = `data:image/webp;base64,${chunk0}${chunk1}${chunk23}${chunk45}${chunk67}${chunk8}`;
+
+export default allPartsAtlasUrl;
