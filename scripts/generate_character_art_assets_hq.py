090d98b47e49738123dcb22fbd5ce949302f67f8
TI-06 <iretaku.shop@gmail.com>
2026-08-04 18:11:08 +0900

    fix: use robust polygons for red and white players

--- scripts/generate_character_art_assets_hq.py
+++ scripts/generate_character_art_assets_hq.py
@@ -16,9 +16,55 @@ spec.loader.exec_module(generator)
 
 original_extract = generator.extract_full_character
 
+POLYGONS = {
+    "higami-ren": [
+        [55, 0],
+        [180, 0],
+        [220, 70],
+        [245, 160],
+        [245, 280],
+        [260, 420],
+        [240, 580],
+        [220, 820],
+        [145, 825],
+        [110, 650],
+        [70, 830],
+        [15, 800],
+        [30, 600],
+        [10, 450],
+        [20, 280],
+        [25, 160],
+    ],
+    "shiroma-minato": [
+        [65, 0],
+        [165, 0],
+        [205, 55],
+        [240, 95],
+        [255, 170],
+        [245, 245],
+        [270, 330],
+        [295, 440],
+        [300, 560],
+        [285, 700],
+        [220, 740],
+        [150, 715],
+        [115, 660],
+        [70, 640],
+        [20, 610],
+        [0, 545],
+        [15, 475],
+        [40, 405],
+        [20, 330],
+        [10, 250],
+        [20, 170],
+        [40, 100],
+    ],
+}
+
 
 def extract_full_character(sheet: Image.Image, character_id: str) -> Image.Image:
-    if character_id != "shiroma-minato":
+    points = POLYGONS.get(character_id)
+    if points is None:
         return original_extract(sheet, character_id)
 
     crop = sheet.crop(generator.FULL_BOXES[character_id]).convert("RGB")
@@ -26,33 +72,7 @@ def extract_full_character(sheet: Image.Image, character_id: str) -> Image.Image
     height, width = rgb.shape[:2]
     mask = np.full((height, width), cv2.GC_BGD, np.uint8)
 
-    polygon = np.array(
-        [
-            [65, 0],
-            [165, 0],
-            [205, 55],
-            [240, 95],
-            [255, 170],
-            [245, 245],
-            [270, 330],
-            [295, 440],
-            [300, 560],
-            [285, 700],
-            [220, 740],
-            [150, 715],
-            [115, 660],
-            [70, 640],
-            [20, 610],
-            [0, 545],
-            [15, 475],
-            [40, 405],
-            [20, 330],
-            [10, 250],
-            [20, 170],
-            [40, 100],
-        ],
-        np.int32,
-    )
+    polygon = np.array(points, np.int32)
     probable = np.zeros((height, width), np.uint8)
     cv2.fillPoly(probable, [polygon], 1)
     mask[probable == 1] = cv2.GC_PR_FGD
@@ -60,7 +80,7 @@ def extract_full_character(sheet: Image.Image, character_id: str) -> Image.Image
     hsv = cv2.cvtColor(rgb, cv2.COLOR_RGB2HSV)
     r, g, b = [rgb[:, :, index].astype(np.int16) for index in range(3)]
     skin = (r > 120) & (g > 60) & (b > 40) & (r > g) & ((r - b) > 18)
-    strong = ((hsv[:, :, 1] > 50) | (hsv[:, :, 2] < 170) | skin) & (
+    strong = ((hsv[:, :, 1] > 45) | (hsv[:, :, 2] < 185) | skin) & (
         probable == 1
     )
     mask[strong] = cv2.GC_FGD
