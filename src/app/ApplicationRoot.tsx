22c44d1540978328667a6808d5633f4e1b5bed3e
TI-06 <iretaku.shop@gmail.com>
2026-08-26 14:44:53 +0900

    feat: gate the application behind authentication

---
+++ src/app/ApplicationRoot.tsx
@@ -0,0 +1,19 @@
+import App from "../App";
+import type { AuthGateway } from "../auth/AuthGateway";
+import { AuthGate } from "../auth/AuthGate";
+import { gameDataBootstrap } from "../data/gameData";
+import { GameDataErrorScreen } from "./GameDataErrorScreen";
+
+interface ApplicationRootProps {
+  authGateway: AuthGateway;
+}
+
+export function ApplicationRoot({ authGateway }: ApplicationRootProps) {
+  if (!gameDataBootstrap.ok) {
+    return <GameDataErrorScreen message={gameDataBootstrap.message} />;
+  }
+
+  return (
+    <AuthGate gateway={authGateway}>{() => <App />}</AuthGate>
+  );
+}
