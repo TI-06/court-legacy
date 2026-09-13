efa5684a865c3b48ba1299fdeba1bd9e01ce56de
TI-06 <iretaku.shop@gmail.com>
2026-09-13 10:07:51 +0900

    refactor: keep main entrypoint refresh-safe

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ApplicationRoot } from "./app/ApplicationRoot";
 import "./app.css";
 import "./features/home/home-week.css";
 import "./mobile-layout.css";
 import "./pwa/app-update-banner.css";
-import { useAppUpdate } from "./pwa/useAppUpdate";
+import { ReleaseAwareApplicationRoot } from "./pwa/ReleaseAwareApplicationRoot";
 import "./ui/theme/game-theme.css";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Application root element was not found");
-
-function ReleaseAwareApplicationRoot() {
-  const { updateReady, refresh } = useAppUpdate();
-  return <ApplicationRoot updateReady={updateReady} onAppRefresh={refresh} />;
-}
 
 createRoot(rootElement).render(
   <StrictMode>
