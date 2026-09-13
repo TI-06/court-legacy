319fa8ccdaabbf282bc43ff78fd6d91ee7b3f31e
TI-06 <iretaku.shop@gmail.com>
2026-09-13 10:04:38 +0900

    feat: activate service worker updates only on request

--- public/sw.js
+++ public/sw.js
@@ -12,6 +12,16 @@ function isCacheableStaticRequest(request) {
   );
 }
 
+self.addEventListener("message", (event) => {
+  if (event.data?.type === "SKIP_WAITING") {
+    void self.skipWaiting();
+  }
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener("fetch", (event) => {
   if (!isCacheableStaticRequest(event.request)) return;
 
