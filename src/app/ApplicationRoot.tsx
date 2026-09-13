c6154786b3be28dc2bb592cca9026d10271dfe35
TI-06 <iretaku.shop@gmail.com>
2026-09-13 10:04:20 +0900

    feat: expose release update banner at app root

--- src/app/ApplicationRoot.tsx
+++ src/app/ApplicationRoot.tsx
@@ -1,5 +1,6 @@
 import App from "../App";
 import { gameDataBootstrap } from "../data/gameData";
+import { AppUpdateBanner } from "../pwa/AppUpdateBanner";
 import type { GameApiClient } from "../services/api/GameApiClient";
 import type { AuthClient } from "../services/auth/AuthClient";
 import { GameDataErrorScreen } from "./GameDataErrorScreen";
@@ -7,10 +8,28 @@ import { GameDataErrorScreen } from "./GameDataErrorScreen";
 interface ApplicationRootProps {
   auth?: AuthClient;
   api?: GameApiClient;
+  updateReady?: boolean;
+  onAppRefresh?: () => void;
 }
 
-export function ApplicationRoot({ auth, api }: ApplicationRootProps) {
-  if (!gameDataBootstrap.ok)
-    return <GameDataErrorScreen message={gameDataBootstrap.message} />;
-  return <App auth={auth} api={api} />;
+const noop = () => undefined;
+
+export function ApplicationRoot({
+  auth,
+  api,
+  updateReady = false,
+  onAppRefresh = noop,
+}: ApplicationRootProps) {
+  const content = gameDataBootstrap.ok ? (
+    <App auth={auth} api={api} />
+  ) : (
+    <GameDataErrorScreen message={gameDataBootstrap.message} />
+  );
+
+  return (
+    <>
+      <AppUpdateBanner updateReady={updateReady} onRefresh={onAppRefresh} />
+      {content}
+    </>
+  );
 }
